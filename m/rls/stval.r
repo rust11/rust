02a7ff037a9bce2d5bdfc3eb13cf600ec8411fe1
rust11 <rust11@users.noreply.github.com>
2022-08-20 00:52:27 +1000

    octal/decimal were switched

--- m/rls/stval.r
+++ m/rls/stval.r
@@ -3,7 +3,7 @@ include rid:rider
 
   func	st_val
 	str : * char		; "1234"
-	bas : int		; 8, 10 or 16 (default is 10, no errors)
+	bas : int		; 8, 10 or 16
 	val : * int	; opt.	; result value
 	cnt : * int~	; opt.	; number of characters parsed
   is	scn : * char
@@ -12,15 +12,15 @@ include rid:rider
 	res : int
 
 	case bas
-	of 8   scn = "%d%n%*s"
-	of 10  scn = "%o%n%*s"
+	of 8   scn = "%o%n%*s"
+	of 10  scn = "%d%n%*s"
 	of 16  scn = "%h%n%*s"
 	of other
 	       fail
 	end case
 	fail if SCN(str, scn, &tmp, &len) lt 2
 ;	res = SCN(str, scn, &tmp, &len)
-;	PUT("cnt=%d len=%d tmp=%d\n", res, len, tmp)
+;	PUT("scn=[%s] cnt=%d len=%d tmp=%d\n", scn,res, len, tmp)
 	*val = tmp if val
 	*cnt = len if cnt
 	fine
